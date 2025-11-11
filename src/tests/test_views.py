c1fafdcd737f6837d1cf2a82909eb96fe9fa2965
artur <artur.spek@gmail.com>
2025-11-11 15:07:19 +0100

    Removes unused import statements from test_views.py

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from fastapi.testclient import TestClient
 
 from database_models import DurationExerciseEntry, RepsExerciseEntry
-from exercises_sources.dtos import AddDurationExercisesRequest, AddRepsExercisesRequest
 from exercises_sources.sql import SQLExerciseSource
 from src.main import app
 

65d931fc82f450674e2c69d4108d254e21615eae
artur <artur.spek@gmail.com>
2026-05-17 18:04:20 +0200

    Fix import statement for `app` in test_views

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -4,7 +4,6 @@ from fastapi.testclient import TestClient
 
 from database_models import DurationExerciseEntry, RepsExerciseEntry
 from exercises_sources.sql import SQLExerciseSource
-from main import app
 from src.main import app
 
 client = TestClient(app)

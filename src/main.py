0603b004c7c46406362ea5e177cca8104a8b0185
artur <artur.spek@gmail.com>
2026-06-06 00:14:16 +0200

    refactor(auth): extract hashing logic to utils

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 from typing import Annotated
-import hashlib
+from src.utils import get_hash
 from dotenv import load_dotenv
 
 import pytz
@@ -48,7 +48,7 @@ async def check_access_key(request: Request):
     access_key = request.headers.get("Authorization")
     if not access_key:
         access_key = request.cookies.get("access_token")
-    hashed = hashlib.sha256(access_key.encode()).hexdigest() if access_key else ""
+    hashed = get_hash(access_key)
     if hashed == os.environ["ACCESS_KEY_HASH"]:
         return True
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid access key")
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,12 @@
+import hashlib
+
+
+def get_hash(input_string: str | None) -> str:
+    if not input_string:
+        return ""
+    return hashlib.sha256(input_string.encode()).hexdigest()
+
+
 def convert_seconds_to_minutes_format(seconds: int) -> str:
     seconds_reminder = seconds % 60
     computed_minutes = seconds // 60
